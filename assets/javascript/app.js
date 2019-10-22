ca792552d9871aee88a59d5a7bccf0a89f5bdac6
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-10-22 18:50:34 -0400

    Added images, laid out html and css, and made array filled with objects containing questions and answers.

---
+++ assets/javascript/app.js
@@ -0,0 +1,64 @@
+var questions = [
+
+    {
+        question: "The Lord of the Rings movies are based on a novel by what author?",
+        choices: ["H.E. Pennypacker", "J.R.R. Tolkien", "H.R. Shovenstuff", "J.G. Ballard"],
+        answer: 1,
+        pic: "assets/images/tolkien.gif",
+    },
+    {
+        question: "What is the first voice we hear in The Fellowship of the Ring?",
+        choices: ["Frodo", "Gandalf", "Bilbo", "Galadriel"],
+        answer: 3,
+        pic: "assets/images/galadriel.gif",
+    },
+    {
+        question: "While traveling through the mines of Moria, which member of the Fellowship of the Ring is killed by the Balrog?",
+        choices: ["Gandalf", "Boromir", "Craig", "Aragorn"],
+        answer: 0,
+        pic: "assets/images/gandalf.gif",
+    },
+    {
+        question: "In Fellowship of the Ring what gift does Lady Galadriel give Gimli before the fellowship leaves Lothlorien?",
+        choices: ["Three strands of hair", "An axe to the face", "Lifts for boots", "A new axe"],
+        answer: 0,
+        pic: "assets/images/threehairs.gif",
+    },
+    {
+        question: "The only way to destroy the Ring of Power is to throw it into the fires of",
+        choices: ["Mount Kilimanjaro", "Mount Mordor", "Mount Doom", "Mount Rushmore"],
+        answer: 2,
+        pic: "assets/images/mountdoom.jpeg",
+    },
+    {
+        question: "What is the name of the Ent who carries Pippin and Merry through Fangorn Forest?",
+        choices: ["Snape", "Treegoatee", "Tyler T. Treely", "Treebeard"],
+        answer: 3,
+        pic: "assets/images/treebeard.gif",
+    },
+    {
+        question: "In The Two Towers, who is Saruman's spy in Rohan?",
+        choices: ["Grima", "Gollum", "Martin Van Buren", "Dr. Van Nostren"],
+        answer: 0,
+        pic: "assets/images/grima.gif",
+    },
+    {
+        question: "In The Return of the King, who kills the Witch King",
+        choices: ["Aragorn", "Pippin", "Eowyn", "Faramir"],
+        answer: 2,
+        pic: "assets/images/eowyn.gif",
+    },
+    {
+        question: "Who does Samwise marry after his return The Shire?",
+        choices: ["Penny Lane", "Rosie Cotton", "Galadriel", "Sally"],
+        answer: 1,
+        pic: "assets/images/rosiecotton.gif",
+    },
+    {
+        question: "How many members were there in the Fellowship of the Ring?",
+        choices: ["Nine", "Three", "Sixteen", "Five"],
+        answer: 0,
+        pic: "assets/images/fellowship.gif",
+    },
+
+]
